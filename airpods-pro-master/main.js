9b1af638e4347ac78ee612298dd76ac2d94b8c91
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-10-18 12:07:29 +0300

    Airpods 3js pages

---
+++ airpods-pro-master/main.js
@@ -0,0 +1,41 @@
+const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+
+const frameCount = 1013;
+const currentFrame = (index) =>
+  `./images/${index.toString().padStart(4, "0")}.jpg`;
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image();
+img.src = currentFrame(1);
+canvas.width = 1158;
+canvas.height = 770;
+img.onload = function () {
+  context.drawImage(img, 0, 0);
+};
+
+const updateImage = (index) => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+};
+
+window.addEventListener("scroll", () => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+
+  requestAnimationFrame(() => updateImage(frameIndex + 1));
+});
+
+preloadImages();
